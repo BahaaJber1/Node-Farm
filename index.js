52678d6a6b95ce5447e9b6ca731d76ea194a184c
BahaaJber1 <bahaajber23@gmail.com>
2025-12-11 23:19:52 +0300

    create simple API that reads data from a file and send it back to the client

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 import fs from "fs";
 import http from "http"; // we gonna use this to implement a simple server
-import url from "url";
+import { dirname } from "path";
+import url, { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log({ __dirname });
+
+// 1- Blocking, synchronous way
+
+// 1. Synchronous read
+//  - Not passing the encoding parameter will return a buffer object which is not human-readable
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log({ textInput });
+
+// 2. Synchronous write
+const textOutput = `This is what we know about the avocado: ${textInput}\nCreated on ${new Date()}`;
+fs.writeFileSync("./txt/output.txt", textOutput);
+console.log("File written!");
+
+// 2- Non-blocking, asynchronous way
+
+// 1. Asynchronous read
+//  - The first argument in the callback is for error handling most of the time
+//  - The second console.log outside the readFile callback will be executed first because readFile is non-blocking meaning it'll be running in the background
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+	console.log({ err, data });
+	if (err)
+		return console.log("Something went wrong while reading this file!", {
+			errorMessage: err,
+		});
+	fs.readFile(`./txt/${data}.txt`, "utf-8", (err1, data1) => {
+		console.log({ err1, data1 });
+		fs.readFile("./txt/append.txt", "utf-8", (err2, data2) => {
+			console.log({ err2, data2 });
+			fs.writeFile("./txt/final.txt", `${data1}\n${data2}`, (err3) => {
+				console.log({ err3 }, "File written!");
+			});
+		});
+	});
+});
+
+console.log("Will read file...");
+
+// Read data for the /api endpoint
+// it can be set as sync because it runs only once when the server starts
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+// 3- Creating a simple server
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 	if (pathName === "/" || pathName === "/overview") {
 		res.end("Welcome to our homepage");
 	} else if (pathName === "/product") {
 		res.end("Welcome to our product page");
+	} else if (pathName === "/api") {
+		// 4- Simple API
+		// its a service that provides data to other applications through endpoints
+		// __dirname is a global variable that holds the path to the current directory in CommonJS modules but not in ES6 modules
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(data);
 	} else {
 		// We can also set headers before sending the response
 		// ? whats a header you say? -> Headers are like metadata for the response, they give additional information about the response being sent to the client
@@ -26,41 +78,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
 	console.log("Listening to requests on port 8000");
 });
-
-/*
-// Blocking, synchronous way
-
-// 1. Synchronous read
-//  - Not passing the encoding parameter will return a buffer object which is not human-readable
-const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log({ textInput });
-
-// 2. Synchronous write
-const textOutput = `This is what we know about the avocado: ${textInput}\nCreated on ${new Date()}`;
-fs.writeFileSync("./txt/output.txt", textOutput);
-console.log("File written!");
-
-// Non-blocking, asynchronous way
-
-// 1. Asynchronous read
-//  - The first argument in the callback is for error handling most of the time
-//  - The second console.log outside the readFile callback will be executed first because readFile is non-blocking meaning it'll be running in the background
-fs.readFile("./txt/start.tt", "utf-8", (err, data) => {
-	console.log({ err, data });
-	if (err)
-		return console.log("Something went wrong while reading this file!", {
-			errorMessage: err,
-		});
-	fs.readFile(`./txt/${data}.txt`, "utf-8", (err1, data1) => {
-		console.log({ err1, data1 });
-		fs.readFile("./txt/append.txt", "utf-8", (err2, data2) => {
-			console.log({ err2, data2 });
-			fs.writeFile("./txt/final.txt", `${data1}\n${data2}`, (err3) => {
-				console.log({ err3 }, "File written!");
-			});
-		});
-	});
-});
-
-console.log("Will read file...");
-*/
